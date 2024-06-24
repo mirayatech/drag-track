04145bf01ee64d4404b4fe177c28d9d954a6f535
〽️iraya <mirayaabrodi@gmail.com>
2024-06-25 00:10:59 +0200

    add: useState for modal visibility and Button component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+import { Button } from "./components";
+
 export default function App() {
-  return <div>Drag Track</div>;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [showAddContainerModal, setShowAddContainerModal] = useState(false);
+
+  return (
+    <div className="mx-auto max-w-7xl py-10">
+      <div className="flex items-center justify-between gap-y-2">
+        <h1 className="text-gray-800 text-3xl font-bold">DragTrack</h1>
+        <Button onClick={() => setShowAddContainerModal(true)} />
+      </div>
+    </div>
+  );
 }
