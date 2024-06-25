29deb6a18f5cd411c83f58d4e786885c43f30f53
〽️iraya <mirayaabrodi@gmail.com>
2024-06-25 05:50:18 +0200

    add: integrate modal component for container addition and refactor useState usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
-import { Button } from "./components";
+import { Button, Modal } from "./components";
 
 export default function App() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [showAddContainerModal, setShowAddContainerModal] = useState(false);
 
   return (
     <div className="mx-auto max-w-7xl py-10">
+      <Modal
+        showModal={showAddContainerModal}
+        setShowModal={setShowAddContainerModal}
+      >
+        <div className="flex flex-col w-full items-start gap-y-4">
+          <h1 className="text-gray-800 text-3xl font-bold">Add Container</h1>
+          <input type="text" />
+
+          <Button onClick={() => {}} label="Add Item" />
+        </div>
+      </Modal>
       <div className="flex items-center justify-between gap-y-2">
         <h1 className="text-gray-800 text-3xl font-bold">DragTrack</h1>
-        <Button onClick={() => setShowAddContainerModal(true)} />
+        <Button
+          onClick={() => setShowAddContainerModal(true)}
+          label="Add Container"
+        />
       </div>
     </div>
   );
