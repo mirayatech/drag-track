20639a737c7b9e4931b0433f84906406aade6a04
〽️iraya <mirayaabrodi@gmail.com>
2024-06-25 05:49:19 +0200

    add: label prop to button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,14 +1,15 @@
 type ButtonProps = {
   onClick: () => void;
+  label: string;
 };
 
-export function Button({ onClick }: ButtonProps) {
+export function Button({ onClick, label }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="flex items-center justify-center rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-gray-900 text-white hover:bg-gray-800 h-10 px-4 py-2"
+      className="flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-gray-900 text-white hover:bg-gray-800 h-10 px-4 py-2"
     >
-      Add Container
+      {label}
     </button>
   );
 }
