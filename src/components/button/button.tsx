16d22324ddffb8e26807466ca3696d02c210505b
〽️iraya <mirayaabrodi@gmail.com>
2024-06-25 06:07:45 +0200

    add: fullWidth prop to Button for conditional width styling

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,13 +1,18 @@
 type ButtonProps = {
   onClick: () => void;
   label: string;
+  fullWidth?: boolean;
 };
 
-export function Button({ onClick, label }: ButtonProps) {
+export function Button({ onClick, label, fullWidth = false }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-gray-900 text-white hover:bg-gray-800 h-10 px-4 py-2"
+      className={`${
+        fullWidth
+          ? "w-full flex items-center justify-center"
+          : "flex items-center justify-center"
+      }  rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-gray-900 text-white hover:bg-gray-800 h-10 px-4 py-2`}
     >
       {label}
     </button>
