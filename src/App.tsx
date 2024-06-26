188e5dc17cc31bb7d93e562ca0da7e370fff2cb7
〽️iraya <mirayaabrodi@gmail.com>
2024-06-26 08:31:40 +0200

    feat: add functionality for dynamic container creation and rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
-import { Button, Input, Modal } from "./components";
+import { Button, Container, Input, Modal } from "./components";
+import { DNDType } from "./lib";
 
 export default function App() {
   const [containerName, setContainerName] = useState("");
+  const [containers, setContainers] = useState<DNDType[]>([]);
   const [showAddContainerModal, setShowAddContainerModal] = useState(false);
+
+  const onAddContainer = () => {
+    if (!containerName) return;
+    const id = `container-${Math.random() * 1000}`;
+    setContainers([
+      ...containers,
+      {
+        id,
+        title: containerName,
+        items: [],
+      },
+    ]);
+    setContainerName("");
+    setShowAddContainerModal(false);
+    console.log(containers);
+  };
 
   return (
     <div className="mx-auto max-w-7xl">
@@ -22,7 +40,11 @@ export default function App() {
             value={containerName}
             onChange={(e) => setContainerName(e.target.value)}
           />
-          <Button fullWidth={true} label="Add container" onClick={() => {}} />
+          <Button
+            fullWidth={true}
+            label="Add container"
+            onClick={onAddContainer}
+          />
         </div>
       </Modal>
 
@@ -32,6 +54,20 @@ export default function App() {
           onClick={() => setShowAddContainerModal(true)}
           label="Add Container"
         />
+      </div>
+      {/* 📦 Containers */}
+      <div className="mt-10">
+        <div className="grid grid-cols-3 gap-6">
+          {containers.map((container) => (
+            <Container
+              id={container.id}
+              title={container.title}
+              key={container.id}
+            >
+              children
+            </Container>
+          ))}
+        </div>
       </div>
     </div>
   );
