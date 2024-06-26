4e8ec6440a3ca2fca2a53ae4e65cc990eac87878
〽️iraya <mirayaabrodi@gmail.com>
2024-06-26 08:27:23 +0200

    feat: add transparent prop to ButtonProps to support transparent styling

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,9 +2,19 @@ type ButtonProps = {
   onClick: () => void;
   label: string;
   fullWidth?: boolean;
+  transparent?: boolean;
 };
 
-export function Button({ onClick, label, fullWidth = false }: ButtonProps) {
+export function Button({
+  onClick,
+  label,
+  fullWidth = false,
+  transparent = false,
+}: ButtonProps) {
+  const transparentStyle = transparent
+    ? "bg-transparent text-gray-900 hover:bg-gray-100"
+    : "bg-gray-900 text-white hover:bg-gray-80";
+
   return (
     <button
       onClick={onClick}
@@ -12,7 +22,7 @@ export function Button({ onClick, label, fullWidth = false }: ButtonProps) {
         fullWidth
           ? "w-full flex items-center justify-center"
           : "flex items-center justify-center"
-      }  rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-gray-900 text-white hover:bg-gray-800 h-10 px-4 py-2`}
+      }  rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${transparentStyle} h-10 px-4 py-2`}
     >
       {label}
     </button>
