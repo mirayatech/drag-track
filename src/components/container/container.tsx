da83622f50383a1dc70db79cac46fec2be860a29
〽️iraya <mirayaabrodi@gmail.com>
2024-06-26 09:46:52 +0200

    feat: enhance Container component with sortable functionality using dnd-Kit

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-import { UniqueIdentifier } from "@dnd-kit/core";
+import { useSortable } from "@dnd-kit/sortable";
 import { GripVertical } from "lucide-react";
+import { CSS } from "@dnd-kit/utilities";
+import clsx from "clsx";
+
+import { UniqueIdentifier } from "@dnd-kit/core";
 import { Button } from "..";
 
 type ContainerProps = {
@@ -10,12 +13,30 @@ type ContainerProps = {
   description?: string;
   onAddItem?: () => void;
 };
-export function Container({ children, title }: ContainerProps) {
+export function Container({ id, children, title }: ContainerProps) {
+  const { attributes, setNodeRef, transform, transition, isDragging } =
+    useSortable({
+      id: id,
+      data: {
+        type: "container",
+      },
+    });
   return (
-    <div className="w-full h-full border bg-gray-50 border-gray-300 rounded-lg flex flex-col gap-y-4">
-      <div className="">
+    <div
+      {...attributes}
+      ref={setNodeRef}
+      style={{
+        transition,
+        transform: CSS.Translate.toString(transform),
+      }}
+      className={clsx(
+        "w-full h-full border bg-gray-50 border-gray-300 rounded-lg flex flex-col gap-y-4",
+        isDragging && "opacity-50"
+      )}
+    >
+      <div>
         <div className="flex items-center bg-white rounded-md-b justify-between rounded-t-lg border-b border-gray-300 p-4">
-          <h1 className="text-gray-800 text-base font-medium">{title}</h1>{" "}
+          <h1 className="text-gray-800 text-base font-medium">{title}</h1>
           <button className="hover:shadow-xl">
             <GripVertical size={16} className="text-gray-400 cursor-grab" />
           </button>
@@ -24,9 +45,9 @@ export function Container({ children, title }: ContainerProps) {
       <div className="flex flex-col gap-y-4 p-4">{children}</div>
       <div className="p-4">
         <Button
-          transparent={true}
           onClick={() => {}}
-          label="Add Item"
+          transparent={true}
+          label=" Add Item"
           fullWidth={true}
         />
       </div>
