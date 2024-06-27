aec0d6a1868e15fd40eb48cf86a56ebb2f26f733
〽️iraya <mirayaabrodi@gmail.com>
2024-06-27 12:07:24 +0200

    add: auto focus functionalities

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,32 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 type InputProps = {
   type: string;
   name: string;
-  placeholder?: string;
   value?: string;
-  rounded?: "rounded-md" | "rounded-lg";
   shadow?: boolean;
+  placeholder?: string;
+  rounded?: "rounded-md" | "rounded-lg";
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export function Input({
   type,
   name,
-  value,
-  placeholder,
-  onChange,
+  value = "",
+  placeholder = "",
+  onChange = () => {},
   rounded = "rounded-md",
   shadow = false,
 }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
   return (
     <input
+      ref={inputRef}
       type={type}
       name={name}
       value={value}
       placeholder={placeholder}
       onChange={onChange}
-      className={`border p-2 w-full ${rounded} ${shadow} `}
-    ></input>
+      className={`border p-2 w-full ${rounded} ${shadow ? "shadow" : ""}`}
+    />
   );
 }
