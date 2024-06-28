6a52049b27cb1b74e9cb3f09ff5275f70f3aa908
〽️iraya <mirayaabrodi@gmail.com>
2024-06-28 08:59:10 +0200

    style: small styling changes

--- src/components/item/item.tsx
+++ src/components/item/item.tsx
@@ -40,34 +40,33 @@ export function Items({ id, title, onEdit, onDelete }: ItemsType) {
         transform: CSS.Translate.toString(transform),
       }}
       className={clsx(
-        "bg-white shadow rounded-md w-full border border-200 hover:border-gray-200 flex pr-2 relative items-start",
-        "bg-white shadow rounded-md w-full border border-200 hover:border-gray-200 flex pr-2 relative items-start",
+        "bg-white shadow rounded-md w-full border  border-200 hover:border-gray-200 flex pr-2 relative",
         isDragging && "opacity-50"
       )}
     >
       <div
-        className="flex flex-1 items-center justify-between p-4 pr-3 max-w-[400px] break-all"
+        className="flex flex-1 items-center justify-between p-4 pr-3"
         {...listeners}
       >
         {title}
-        <button
-          className="text-gray-600 hover:text-gray-900 transition-colors "
-          onClick={() => setMenuOpen(!isMenuOpen)}
-        >
-          <EllipsisVertical size={17} />
-        </button>
-        <AnimatePresence>
-          {isMenuOpen && (
-            <Menu
-              extraTop={true}
-              setMenuOpen={setMenuOpen}
-              isMenuOpen={isMenuOpen}
-              onEdit={onEdit}
-              onDelete={onDelete}
-            />
-          )}
-        </AnimatePresence>
       </div>
+      <button
+        className="text-gray-600 hover:text-gray-900 transition-colors"
+        onClick={() => setMenuOpen(!isMenuOpen)}
+      >
+        <EllipsisVertical size={17} />
+      </button>
+      <AnimatePresence>
+        {isMenuOpen && (
+          <Menu
+            extraTop={true}
+            setMenuOpen={setMenuOpen}
+            isMenuOpen={isMenuOpen}
+            onEdit={onEdit}
+            onDelete={onDelete}
+          />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
