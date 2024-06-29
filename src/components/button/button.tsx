d721779678d3d1da6094599cd95f49ed7f1409ab
〽️iraya <mirayaabrodi@gmail.com>
2024-06-29 22:15:58 +0200

    style: ensure bgLight prop works correctly and refactor className handling

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,7 +5,6 @@ type ButtonProps = {
   label: string;
   fullWidth?: boolean;
   bgLight?: boolean;
-  danger?: boolean;
   variant?:
     | "default"
     | "destructive"
@@ -23,11 +22,18 @@ export function Button({
   fullWidth = false,
   variant = "default",
   bgLight = false,
-  danger = false,
   disabled = false,
   icon: Icon,
 }: ButtonProps) {
-  const buttonVariants = {
+  const baseClasses =
+    "flex items-center justify-center gap-3 rounded md:rounded-md text-xs font-bold md:text-sm md:font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 py-2 px-3 md:px-4 md:py-2";
+
+  const fullWidthClass = fullWidth ? "w-full text-center" : "";
+  const bgLightClass = bgLight
+    ? "bg-indigo-100 hover:bg-indigo-200 text-indigo-600 hover:text-indigo-800"
+    : "";
+
+  const variantClasses = {
     default: "bg-indigo-500 text-white hover:bg-indigo-600",
     destructive: "bg-red-600 text-white hover:bg-red-700",
     outline: "border border-slate-300 bg-white hover:bg-slate-100",
@@ -37,22 +43,12 @@ export function Button({
     link: "text-blue-600 underline hover:text-blue-800",
   };
 
-  const dangerClass = "bg-red-500 text-white hover:bg-red-600";
-
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`${
-        fullWidth
-          ? "w-full flex items-center justify-center text-center"
-          : "flex items-center justify-center "
-      } gap-3 rounded md:rounded-md text-xs font-bold md:text-sm md:font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${
-        danger ? dangerClass : buttonVariants[variant]
-      } md:h-10 py-2 px-3 md:px-4 md:py-2 ${
-        bgLight && !danger
-          ? "bg-indigo-100 hover:bg-indigo-200 text-indigo-600 hover:text-indigo-800"
-          : ""
+      className={`${baseClasses} ${fullWidthClass} ${bgLightClass} ${
+        !bgLight && variantClasses[variant]
       }`}
     >
       {Icon && <Icon size={18} />}
